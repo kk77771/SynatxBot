6631769c77f4597408a95a107575b4f49661604f
ori <ori.mizrahi1000@gmail.com>
2021-08-14 00:57:03 +0300

    Created artifact command and changed the way of changing to color for embed.

--- src/cogs/genshin.py
+++ src/cogs/genshin.py
@@ -7,6 +7,7 @@ class Genshin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         logger.info("Genshin impact Cog loaded")
+        self.colour_dict = {5: discord.Colour.gold(), 4: discord.Colour.purple(), 3: discord.Colour.blue(), 2: discord.Colour.green(), 1: discord.Colour.light_gray()}
 
     @commands.group(invoke_without_command=True, description="Genshin impact group commands.")
     @commands.check(private_message)
@@ -19,7 +20,7 @@ class Genshin(commands.Cog):
         if response.status_code == 404:
             raise commands.BadArgument(message=f"no character named {character}.")
         json = response.json()
-        embed = discord.Embed(title=f"{json['name']} information", description=json["description"], colour=discord.Colour.gold() if json["rarity"] == 5 else discord.Colour.purple())
+        embed = discord.Embed(title=f"{json['name']} information", description=json["description"], colour=self.colour_dict[json["rarity"]])
         embed.add_field(name="Vision", value=json["vision"], inline=True)
         embed.add_field(name="Nation", value=json["nation"], inline=True)
         embed.add_field(name="Affiliation", value=json["affiliation"], inline=True)
@@ -30,6 +31,23 @@ class Genshin(commands.Cog):
 
     @character.error
     async def character_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send(f"Error: `{error}`")
+
+    @genshin.group(name="artifact", description="Sending genshin impact artifact data")
+    async def artifact(self, ctx, artifact: str):
+        response = requests.get(f"https://api.genshin.dev/artifacts/{artifact.lower()}")
+        if response.status_code == 404:
+            raise commands.BadArgument(message=f"no artifact named {artifact}.")
+        json = response.json()
+        embed = discord.Embed(title=f"{json['name']} information", description=json["description"], colour=self.colour_dict[json["max_rarity"]])
+        embed.add_field(name="Max rarity", value=json["max_rarity"], inline=True)
+        embed.add_field(name="2 piece bonus", value=json["2-piece_bonus"], inline=True)
+        embed.add_field(name="4 piece bonus", value=json["4-piece_bonu"], inline=True)
+        await ctx.send(embed=embed)
+
+    @artifact.error
+    async def artifact_error(self, ctx, error):
         if isinstance(error, commands.BadArgument):
             await ctx.send(f"Error: `{error}`")
     
